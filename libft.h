54d30a2554250540ec1660fa3d94beec57208e76
vincent lafouasse <poss@possmbp.home>
2023-08-26 15:54:23 +0200

    add ft prefix

--- libft.h
+++ libft.h
@@ -6,44 +6,44 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/26 15:36:41 by poss              #+#    #+#             */
-/*   Updated: 2023/08/26 15:51:02 by poss             ###   ########.fr       */
+/*   Updated: 2023/08/26 15:53:18 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* from `stdlib.h` */
-int		atoi(const char *nptr);
+int		ft_atoi(const char *nptr);
 
 /* from `stdlib.h` */
-int		isalpha(int);
-int		isdigit(int);
-int		isalnum(int);
-int		toascii(int);
-int		isprint(int);
-int		tolower(int);
-int		toupper(int);
+int		ft_isalpha(int);
+int		ft_isdigit(int);
+int		ft_isalnum(int);
+int		ft_toascii(int);
+int		ft_isprint(int);
+int		ft_tolower(int);
+int		ft_toupper(int);
 
 /* from `string.h` */
-void	*memset(void *, int, size_t);
-void	*memcpy(void *dest, const void *src, size_t n);
-void	*memmove(void *dest, const void *src, size_t n);
-void	*memchr(const void *s, int c, size_t n);
-int		memcmp(const void *s1, const void *s2, size_t n);
-char	*strcat(char *dest, const char *src);
-char	*strncat(char *dest, const char *src, size_t n);
-char	*strchr(const char *, int);
-char	*strrchr(const char *, int);
-int		strcmp(const char *, const char *);
-int		strncmp(const char *, const char *, size_t n);
-char	*strcpy(char *toHere, const char *fromHere);
-char	*strncpy(char *toHere, const char *fromHere, size_t n);
-size_t	strlen(const char *);
-char	*strstr(const char *haystack, const char *needle);
-char	*strdup(const char *);
-void	*memccpy(void *dest, const void *src, int c, size_t n);
-char	*strnstr(const char *big, const char *little, size_t len);
+void	*ft_memset(void *, int, size_t);
+void	*ft_memcpy(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int		ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strcat(char *dest, const char *src);
+char	*ft_strncat(char *dest, const char *src, size_t n);
+char	*ft_strchr(const char *, int);
+char	*ft_strrchr(const char *, int);
+int		ft_strcmp(const char *, const char *);
+int		ft_strncmp(const char *, const char *, size_t n);
+char	*ft_strcpy(char *toHere, const char *fromHere);
+char	*ft_strncpy(char *toHere, const char *fromHere, size_t n);
+size_t	ft_strlen(const char *);
+char	*ft_strstr(const char *haystack, const char *needle);
+char	*ft_strdup(const char *);
+void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
 
 /* from `strings.h` */
-void	bzero(void *s, size_t n);
+void	ft_bzero(void *s, size_t n);
 
 /* from `bsd/string.h` */
-size_t	strlcat(char *dst, const char *src, size_t size);
+size_t	ft_strlcat(char *dst, const char *src, size_t size);
