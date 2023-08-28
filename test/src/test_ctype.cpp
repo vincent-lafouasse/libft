c0cb8d10680e9c219fbd898ea8db9c2db5c769ea
vincent lafouasse <poss@possmbp.home>
2023-08-28 12:07:55 +0200

    test isdigit

--- test/src/test_ctype.cpp
+++ test/src/test_ctype.cpp
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/27 21:24:35 by poss              #+#    #+#             */
-/*   Updated: 2023/08/27 21:25:05 by poss             ###   ########.fr       */
+/*   Updated: 2023/08/28 12:07:37 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,24 +17,39 @@ extern "C" {
 };
 
 TEST(Ctype, IsSpace) {
-  EXPECT_TRUE(ft_isspace(' '));
-  EXPECT_TRUE(ft_isspace('\r'));
-  EXPECT_TRUE(ft_isspace('\t'));
-  EXPECT_TRUE(ft_isspace('\n'));
-  EXPECT_TRUE(ft_isspace('\v'));
-  EXPECT_TRUE(ft_isspace('\f'));
+	EXPECT_TRUE(ft_isspace(' '));
+	EXPECT_TRUE(ft_isspace('\r'));
+	EXPECT_TRUE(ft_isspace('\t'));
+	EXPECT_TRUE(ft_isspace('\n'));
+	EXPECT_TRUE(ft_isspace('\v'));
+	EXPECT_TRUE(ft_isspace('\f'));
 
-  EXPECT_FALSE(ft_isspace('\0'));
-  EXPECT_FALSE(ft_isspace('\a'));
-  EXPECT_FALSE(ft_isspace('\b'));
-  EXPECT_FALSE(ft_isspace('4'));
-  EXPECT_FALSE(ft_isspace('2'));
-  EXPECT_FALSE(ft_isspace('0'));
-  EXPECT_FALSE(ft_isspace('m'));
-  EXPECT_FALSE(ft_isspace('d'));
-  EXPECT_FALSE(ft_isspace('r'));
+	EXPECT_FALSE(ft_isspace('\0'));
+	EXPECT_FALSE(ft_isspace('\a'));
+	EXPECT_FALSE(ft_isspace('\b'));
+	EXPECT_FALSE(ft_isspace('4'));
+	EXPECT_FALSE(ft_isspace('2'));
+	EXPECT_FALSE(ft_isspace('0'));
+	EXPECT_FALSE(ft_isspace('m'));
+	EXPECT_FALSE(ft_isspace('d'));
+	EXPECT_FALSE(ft_isspace('r'));
 
-  EXPECT_FALSE(ft_isspace(-1));
-  EXPECT_FALSE(ft_isspace(127));
-  EXPECT_FALSE(ft_isspace(255));
+	EXPECT_FALSE(ft_isspace(-1));
+	EXPECT_FALSE(ft_isspace(127));
+	EXPECT_FALSE(ft_isspace(255));
+}
+
+TEST(Ctype, IsDigit) {
+	EXPECT_TRUE(ft_isdigit('4'));
+	EXPECT_TRUE(ft_isdigit('2'));
+	EXPECT_TRUE(ft_isdigit('0'));
+
+	EXPECT_FALSE(ft_isdigit('m'));
+	EXPECT_FALSE(ft_isdigit('d'));
+	EXPECT_FALSE(ft_isdigit('r'));
+	EXPECT_FALSE(ft_isdigit('\0'));
+
+	EXPECT_FALSE(ft_isdigit(-1));
+	EXPECT_FALSE(ft_isdigit(127));
+	EXPECT_FALSE(ft_isdigit(255));
 }
