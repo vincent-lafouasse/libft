bdb5249a7f6b842cfd4b13ae25fda3f2e1c93c5f
vincent lafouasse <poss@possmbp.home>
2023-09-02 23:26:43 +0200

    format c++ in a chromium-ish style

--- test/src/test_ctype.cpp
+++ test/src/test_ctype.cpp
@@ -12,44 +12,47 @@
 
 #include <gtest/gtest.h>
 
-extern "C" {
+extern "C"
+{
 #include "libft.h"
 };
 
-TEST(Ctype, IsSpace) {
-	EXPECT_TRUE(ft_isspace(' '));
-	EXPECT_TRUE(ft_isspace('\r'));
-	EXPECT_TRUE(ft_isspace('\t'));
-	EXPECT_TRUE(ft_isspace('\n'));
-	EXPECT_TRUE(ft_isspace('\v'));
-	EXPECT_TRUE(ft_isspace('\f'));
+TEST(Ctype, IsSpace)
+{
+  EXPECT_TRUE(ft_isspace(' '));
+  EXPECT_TRUE(ft_isspace('\r'));
+  EXPECT_TRUE(ft_isspace('\t'));
+  EXPECT_TRUE(ft_isspace('\n'));
+  EXPECT_TRUE(ft_isspace('\v'));
+  EXPECT_TRUE(ft_isspace('\f'));
 
-	EXPECT_FALSE(ft_isspace('\0'));
-	EXPECT_FALSE(ft_isspace('\a'));
-	EXPECT_FALSE(ft_isspace('\b'));
-	EXPECT_FALSE(ft_isspace('4'));
-	EXPECT_FALSE(ft_isspace('2'));
-	EXPECT_FALSE(ft_isspace('0'));
-	EXPECT_FALSE(ft_isspace('m'));
-	EXPECT_FALSE(ft_isspace('d'));
-	EXPECT_FALSE(ft_isspace('r'));
+  EXPECT_FALSE(ft_isspace('\0'));
+  EXPECT_FALSE(ft_isspace('\a'));
+  EXPECT_FALSE(ft_isspace('\b'));
+  EXPECT_FALSE(ft_isspace('4'));
+  EXPECT_FALSE(ft_isspace('2'));
+  EXPECT_FALSE(ft_isspace('0'));
+  EXPECT_FALSE(ft_isspace('m'));
+  EXPECT_FALSE(ft_isspace('d'));
+  EXPECT_FALSE(ft_isspace('r'));
 
-	EXPECT_FALSE(ft_isspace(-1));
-	EXPECT_FALSE(ft_isspace(127));
-	EXPECT_FALSE(ft_isspace(255));
+  EXPECT_FALSE(ft_isspace(-1));
+  EXPECT_FALSE(ft_isspace(127));
+  EXPECT_FALSE(ft_isspace(255));
 }
 
-TEST(Ctype, IsDigit) {
-	EXPECT_TRUE(ft_isdigit('4'));
-	EXPECT_TRUE(ft_isdigit('2'));
-	EXPECT_TRUE(ft_isdigit('0'));
+TEST(Ctype, IsDigit)
+{
+  EXPECT_TRUE(ft_isdigit('4'));
+  EXPECT_TRUE(ft_isdigit('2'));
+  EXPECT_TRUE(ft_isdigit('0'));
 
-	EXPECT_FALSE(ft_isdigit('m'));
-	EXPECT_FALSE(ft_isdigit('d'));
-	EXPECT_FALSE(ft_isdigit('r'));
-	EXPECT_FALSE(ft_isdigit('\0'));
+  EXPECT_FALSE(ft_isdigit('m'));
+  EXPECT_FALSE(ft_isdigit('d'));
+  EXPECT_FALSE(ft_isdigit('r'));
+  EXPECT_FALSE(ft_isdigit('\0'));
 
-	EXPECT_FALSE(ft_isdigit(-1));
-	EXPECT_FALSE(ft_isdigit(127));
-	EXPECT_FALSE(ft_isdigit(255));
+  EXPECT_FALSE(ft_isdigit(-1));
+  EXPECT_FALSE(ft_isdigit(127));
+  EXPECT_FALSE(ft_isdigit(255));
 }
