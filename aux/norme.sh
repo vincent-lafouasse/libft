77e45b006fad2f29867490b965ce59833b4a828d
vincent lafouasse <poss@possmbp.home>
2023-09-05 19:26:34 +0200

    avoid globbing

--- aux/norme.sh
+++ aux/norme.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 function norm {
-	echo Formatting $(basename $1)
-    python3 -m c_formatter_42 < $1 > temp
-    mv temp $1
+	echo Formatting "$(basename "$1")"
+	python3 -m c_formatter_42 < "$1" >temp
+	mv temp "$1"
 }
 
 SRC_DIR='./src'
@@ -15,5 +15,5 @@ H_FILES=$(find $H_DIR -name '*.h')
 TO_NORM="$C_FILES $H_FILES"
 
 for file in $TO_NORM; do
-    norm $file
+	norm "$file"
 done
