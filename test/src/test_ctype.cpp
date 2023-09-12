f1e24d7e4b37dce5438923524429c3537e8baf6b
vincent lafouasse <poss@possmbp.home>
2023-09-12 19:00:07 +0200

    DRYer tests

--- test/src/test_ctype.cpp
+++ test/src/test_ctype.cpp
@@ -10,8 +10,8 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <ctype.h>
 #include <gtest/gtest.h>
-#include <cstdio>
 
 extern "C"
 {
@@ -26,225 +26,265 @@ extern "C"
 
 // Poss extension ----------------------
 
+void test_isupper(int c)
+{
+  EXPECT_EQ(ft_isupper(c), isupper(c));
+}
+
+void test_islower(int c)
+{
+  EXPECT_EQ(ft_islower(c), islower(c));
+}
+
+void test_isspace(int c)
+{
+  EXPECT_EQ(ft_isspace(c), isspace(c));
+}
+
+void test_isalpha(int c)
+{
+  EXPECT_EQ(ft_isalpha(c), isalpha(c));
+}
+
+void test_isdigit(int c)
+{
+  EXPECT_EQ(ft_isdigit(c), isdigit(c));
+}
+
+void test_isalnum(int c)
+{
+  EXPECT_EQ(ft_isalnum(c), isalnum(c));
+}
+
+void test_isascii(int c)
+{
+  EXPECT_EQ(ft_isascii(c), isascii(c));
+}
+
+void test_isprint(int c)
+{
+  EXPECT_EQ(ft_isprint(c), isprint(c));
+}
+
 TEST(Ctype, IsUpper)
 {
-  EXPECT_TRUE(ft_isupper('B'));
-  EXPECT_TRUE(ft_isupper('O'));
-  EXPECT_TRUE(ft_isupper('N'));
-  EXPECT_TRUE(ft_isupper('J'));
-  EXPECT_TRUE(ft_isupper('O'));
-  EXPECT_TRUE(ft_isupper('U'));
-  EXPECT_TRUE(ft_isupper('R'));
+  test_isupper('B');
+  test_isupper('O');
+  test_isupper('N');
+  test_isupper('J');
+  test_isupper('O');
+  test_isupper('U');
+  test_isupper('R');
 
-  EXPECT_FALSE(ft_isupper('4'));
-  EXPECT_FALSE(ft_isupper('2'));
-  EXPECT_FALSE(ft_isupper('0'));
-  EXPECT_FALSE(ft_isupper('b'));
-  EXPECT_FALSE(ft_isupper('o'));
-  EXPECT_FALSE(ft_isupper('n'));
-  EXPECT_FALSE(ft_isupper('j'));
-  EXPECT_FALSE(ft_isupper('o'));
-  EXPECT_FALSE(ft_isupper('u'));
-  EXPECT_FALSE(ft_isupper('r'));
-  EXPECT_FALSE(ft_isupper(EOT));
-  EXPECT_FALSE(ft_isupper(BEL));
-  EXPECT_FALSE(ft_isupper(ESC));
-  EXPECT_FALSE(ft_isupper(DEL));
-  EXPECT_FALSE(ft_isupper('\0'));
+  test_isupper('4');
+  test_isupper('2');
+  test_isupper('0');
+  test_isupper('b');
+  test_isupper('o');
+  test_isupper('n');
+  test_isupper('j');
+  test_isupper('o');
+  test_isupper('u');
+  test_isupper('r');
+  test_isupper(EOT);
+  test_isupper(BEL);
+  test_isupper(ESC);
+  test_isupper(DEL);
+  test_isupper('\0');
 
-  EXPECT_FALSE(ft_isupper(EOF_));
-  EXPECT_FALSE(ft_isupper(420));
+  test_isupper(EOF_);
+  test_isupper(420);
 }
 
 TEST(Ctype, IsLower)
 {
-  EXPECT_TRUE(ft_islower('b'));
-  EXPECT_TRUE(ft_islower('o'));
-  EXPECT_TRUE(ft_islower('n'));
-  EXPECT_TRUE(ft_islower('j'));
-  EXPECT_TRUE(ft_islower('o'));
-  EXPECT_TRUE(ft_islower('u'));
-  EXPECT_TRUE(ft_islower('r'));
+  test_islower('b');
+  test_islower('o');
+  test_islower('n');
+  test_islower('j');
+  test_islower('o');
+  test_islower('u');
+  test_islower('r');
 
-  EXPECT_FALSE(ft_islower('4'));
-  EXPECT_FALSE(ft_islower('2'));
-  EXPECT_FALSE(ft_islower('0'));
-  EXPECT_FALSE(ft_islower('B'));
-  EXPECT_FALSE(ft_islower('O'));
-  EXPECT_FALSE(ft_islower('N'));
-  EXPECT_FALSE(ft_islower('J'));
-  EXPECT_FALSE(ft_islower('O'));
-  EXPECT_FALSE(ft_islower('U'));
-  EXPECT_FALSE(ft_islower('R'));
-  EXPECT_FALSE(ft_islower(EOT));
-  EXPECT_FALSE(ft_islower(BEL));
-  EXPECT_FALSE(ft_islower(ESC));
-  EXPECT_FALSE(ft_islower(DEL));
-  EXPECT_FALSE(ft_islower('\0'));
+  test_islower('4');
+  test_islower('2');
+  test_islower('0');
+  test_islower('B');
+  test_islower('O');
+  test_islower('N');
+  test_islower('J');
+  test_islower('O');
+  test_islower('U');
+  test_islower('R');
+  test_islower(EOT);
+  test_islower(BEL);
+  test_islower(ESC);
+  test_islower(DEL);
+  test_islower('\0');
 
-  EXPECT_FALSE(ft_islower(EOF_));
-  EXPECT_FALSE(ft_islower(420));
+  test_islower(EOF_);
+  test_islower(420);
 }
 
 TEST(Ctype, IsSpace)
 {
-  EXPECT_TRUE(ft_isspace(' '));
-  EXPECT_TRUE(ft_isspace('\r'));
-  EXPECT_TRUE(ft_isspace('\t'));
-  EXPECT_TRUE(ft_isspace('\n'));
-  EXPECT_TRUE(ft_isspace('\v'));
-  EXPECT_TRUE(ft_isspace('\f'));
+  test_isspace(' ');
+  test_isspace('\r');
+  test_isspace('\t');
+  test_isspace('\n');
+  test_isspace('\v');
+  test_isspace('\f');
 
-  EXPECT_FALSE(ft_isspace('4'));
-  EXPECT_FALSE(ft_isspace('2'));
-  EXPECT_FALSE(ft_isspace('0'));
-  EXPECT_FALSE(ft_isspace('B'));
-  EXPECT_FALSE(ft_isspace('o'));
-  EXPECT_FALSE(ft_isspace('n'));
-  EXPECT_FALSE(ft_isspace('j'));
-  EXPECT_FALSE(ft_isspace('o'));
-  EXPECT_FALSE(ft_isspace('u'));
-  EXPECT_FALSE(ft_isspace('r'));
-  EXPECT_FALSE(ft_isspace('L'));
-  EXPECT_FALSE(ft_isspace('o'));
-  EXPECT_FALSE(ft_isspace('l'));
-  EXPECT_FALSE(ft_isspace(EOT));
-  EXPECT_FALSE(ft_isspace(BEL));
-  EXPECT_FALSE(ft_isspace(ESC));
-  EXPECT_FALSE(ft_isspace(DEL));
-  EXPECT_FALSE(ft_isspace('\0'));
+  test_isspace('4');
+  test_isspace('2');
+  test_isspace('0');
+  test_isspace('B');
+  test_isspace('o');
+  test_isspace('n');
+  test_isspace('j');
+  test_isspace('o');
+  test_isspace('u');
+  test_isspace('r');
+  test_isspace('L');
+  test_isspace('o');
+  test_isspace('l');
+  test_isspace(EOT);
+  test_isspace(BEL);
+  test_isspace(ESC);
+  test_isspace(DEL);
+  test_isspace('\0');
 
-  EXPECT_FALSE(ft_isspace(EOF_));
-  EXPECT_FALSE(ft_isspace(420));
+  test_isspace(EOF_);
+  test_isspace(420);
 }
 
 // Lib functions
 
 TEST(Ctype, IsAlpha)
 {
-  EXPECT_TRUE(ft_isalpha('B'));
-  EXPECT_TRUE(ft_isalpha('o'));
-  EXPECT_TRUE(ft_isalpha('n'));
-  EXPECT_TRUE(ft_isalpha('j'));
-  EXPECT_TRUE(ft_isalpha('o'));
-  EXPECT_TRUE(ft_isalpha('u'));
-  EXPECT_TRUE(ft_isalpha('r'));
-  EXPECT_TRUE(ft_isalpha('L'));
-  EXPECT_TRUE(ft_isalpha('o'));
-  EXPECT_TRUE(ft_isalpha('l'));
+  test_isalpha('B');
+  test_isalpha('o');
+  test_isalpha('n');
+  test_isalpha('j');
+  test_isalpha('o');
+  test_isalpha('u');
+  test_isalpha('r');
+  test_isalpha('L');
+  test_isalpha('o');
+  test_isalpha('l');
 
-  EXPECT_FALSE(ft_isalpha('4'));
-  EXPECT_FALSE(ft_isalpha('2'));
-  EXPECT_FALSE(ft_isalpha('0'));
-  EXPECT_FALSE(ft_isalpha(EOT));
-  EXPECT_FALSE(ft_isalpha(BEL));
-  EXPECT_FALSE(ft_isalpha(ESC));
-  EXPECT_FALSE(ft_isalpha(DEL));
-  EXPECT_FALSE(ft_isalpha('\0'));
+  test_isalpha('4');
+  test_isalpha('2');
+  test_isalpha('0');
+  test_isalpha(EOT);
+  test_isalpha(BEL);
+  test_isalpha(ESC);
+  test_isalpha(DEL);
+  test_isalpha('\0');
 
-  EXPECT_FALSE(ft_isalpha(EOF_));
-  EXPECT_FALSE(ft_isalpha(420));
+  test_isalpha(EOF_);
+  test_isalpha(420);
 }
 
 TEST(Ctype, IsDigit)
 {
-  EXPECT_TRUE(ft_isdigit('4'));
-  EXPECT_TRUE(ft_isdigit('2'));
-  EXPECT_TRUE(ft_isdigit('0'));
+  test_isdigit('4');
+  test_isdigit('2');
+  test_isdigit('0');
 
-  EXPECT_FALSE(ft_isdigit('B'));
-  EXPECT_FALSE(ft_isdigit('o'));
-  EXPECT_FALSE(ft_isdigit('n'));
-  EXPECT_FALSE(ft_isdigit('j'));
-  EXPECT_FALSE(ft_isdigit('o'));
-  EXPECT_FALSE(ft_isdigit('u'));
-  EXPECT_FALSE(ft_isdigit('r'));
-  EXPECT_FALSE(ft_isdigit('L'));
-  EXPECT_FALSE(ft_isdigit('o'));
-  EXPECT_FALSE(ft_isdigit('l'));
-  EXPECT_FALSE(ft_isdigit(EOT));
-  EXPECT_FALSE(ft_isdigit(BEL));
-  EXPECT_FALSE(ft_isdigit(ESC));
-  EXPECT_FALSE(ft_isdigit(DEL));
-  EXPECT_FALSE(ft_isdigit('\0'));
+  test_isdigit('B');
+  test_isdigit('o');
+  test_isdigit('n');
+  test_isdigit('j');
+  test_isdigit('o');
+  test_isdigit('u');
+  test_isdigit('r');
+  test_isdigit('L');
+  test_isdigit('o');
+  test_isdigit('l');
+  test_isdigit(EOT);
+  test_isdigit(BEL);
+  test_isdigit(ESC);
+  test_isdigit(DEL);
+  test_isdigit('\0');
 
-  EXPECT_FALSE(ft_isdigit(EOF_));
-  EXPECT_FALSE(ft_isdigit(420));
+  test_isdigit(EOF_);
+  test_isdigit(420);
 }
 
 TEST(Ctype, IsAlnum)
 {
-  EXPECT_TRUE(ft_isalnum('4'));
-  EXPECT_TRUE(ft_isalnum('2'));
-  EXPECT_TRUE(ft_isalnum('0'));
-  EXPECT_TRUE(ft_isalnum('B'));
-  EXPECT_TRUE(ft_isalnum('o'));
-  EXPECT_TRUE(ft_isalnum('n'));
-  EXPECT_TRUE(ft_isalnum('j'));
-  EXPECT_TRUE(ft_isalnum('o'));
-  EXPECT_TRUE(ft_isalnum('u'));
-  EXPECT_TRUE(ft_isalnum('r'));
-  EXPECT_TRUE(ft_isalnum('L'));
-  EXPECT_TRUE(ft_isalnum('o'));
-  EXPECT_TRUE(ft_isalnum('l'));
+  test_isalnum('4');
+  test_isalnum('2');
+  test_isalnum('0');
+  test_isalnum('B');
+  test_isalnum('o');
+  test_isalnum('n');
+  test_isalnum('j');
+  test_isalnum('o');
+  test_isalnum('u');
+  test_isalnum('r');
+  test_isalnum('L');
+  test_isalnum('o');
+  test_isalnum('l');
 
-  EXPECT_FALSE(ft_isalnum(EOT));
-  EXPECT_FALSE(ft_isalnum(BEL));
-  EXPECT_FALSE(ft_isalnum(ESC));
-  EXPECT_FALSE(ft_isalnum(DEL));
-  EXPECT_FALSE(ft_isalnum('\0'));
+  test_isalnum(EOT);
+  test_isalnum(BEL);
+  test_isalnum(ESC);
+  test_isalnum(DEL);
+  test_isalnum('\0');
 
-  EXPECT_FALSE(ft_isalnum(EOF_));
-  EXPECT_FALSE(ft_isalnum(420));
+  test_isalnum(EOF_);
+  test_isalnum(420);
 }
 
 TEST(Ctype, IsASCII)
 {
-  EXPECT_TRUE(ft_isascii('4'));
-  EXPECT_TRUE(ft_isascii('2'));
-  EXPECT_TRUE(ft_isascii('0'));
-  EXPECT_TRUE(ft_isascii('B'));
-  EXPECT_TRUE(ft_isascii('o'));
-  EXPECT_TRUE(ft_isascii('n'));
-  EXPECT_TRUE(ft_isascii('j'));
-  EXPECT_TRUE(ft_isascii('o'));
-  EXPECT_TRUE(ft_isascii('u'));
-  EXPECT_TRUE(ft_isascii('r'));
-  EXPECT_TRUE(ft_isascii('L'));
-  EXPECT_TRUE(ft_isascii('o'));
-  EXPECT_TRUE(ft_isascii('l'));
-  EXPECT_TRUE(ft_isascii(EOT));
-  EXPECT_TRUE(ft_isascii(BEL));
-  EXPECT_TRUE(ft_isascii(ESC));
-  EXPECT_TRUE(ft_isascii(DEL));
-  EXPECT_TRUE(ft_isascii('\0'));
+  test_isascii('4');
+  test_isascii('2');
+  test_isascii('0');
+  test_isascii('B');
+  test_isascii('o');
+  test_isascii('n');
+  test_isascii('j');
+  test_isascii('o');
+  test_isascii('u');
+  test_isascii('r');
+  test_isascii('L');
+  test_isascii('o');
+  test_isascii('l');
+  test_isascii(EOT);
+  test_isascii(BEL);
+  test_isascii(ESC);
+  test_isascii(DEL);
+  test_isascii('\0');
 
-  EXPECT_FALSE(ft_isascii(EOF_));
-  EXPECT_FALSE(ft_isascii(420));
+  test_isascii(EOF_);
+  test_isascii(420);
 }
 
 TEST(Ctype, IsPrint)
 {
-  EXPECT_TRUE(ft_isprint('4'));
-  EXPECT_TRUE(ft_isprint('2'));
-  EXPECT_TRUE(ft_isprint('0'));
-  EXPECT_TRUE(ft_isprint('B'));
-  EXPECT_TRUE(ft_isprint('o'));
-  EXPECT_TRUE(ft_isprint('n'));
-  EXPECT_TRUE(ft_isprint('j'));
-  EXPECT_TRUE(ft_isprint('o'));
-  EXPECT_TRUE(ft_isprint('u'));
-  EXPECT_TRUE(ft_isprint('r'));
-  EXPECT_TRUE(ft_isprint('L'));
-  EXPECT_TRUE(ft_isprint('o'));
-  EXPECT_TRUE(ft_isprint('l'));
+  test_isprint('4');
+  test_isprint('2');
+  test_isprint('0');
+  test_isprint('B');
+  test_isprint('o');
+  test_isprint('n');
+  test_isprint('j');
+  test_isprint('o');
+  test_isprint('u');
+  test_isprint('r');
+  test_isprint('L');
+  test_isprint('o');
+  test_isprint('l');
 
-  EXPECT_FALSE(ft_isprint(EOT));
-  EXPECT_FALSE(ft_isprint(BEL));
-  EXPECT_FALSE(ft_isprint(ESC));
-  EXPECT_FALSE(ft_isprint(DEL));
-  EXPECT_FALSE(ft_isprint('\0'));
+  test_isprint(EOT);
+  test_isprint(BEL);
+  test_isprint(ESC);
+  test_isprint(DEL);
+  test_isprint('\0');
 
-  EXPECT_FALSE(ft_isprint(EOF_));
-  EXPECT_FALSE(ft_isprint(420));
+  test_isprint(EOF_);
+  test_isprint(420);
 }
