e5d52de2b49cf81a117d8c4845179baa790f2dc9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-07 13:28:01 +0100

    implement calloc

--- src/libc/stdlib/ft_calloc.c
+++ src/libc/stdlib/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:12:21 by vlafouas          #+#    #+#             */
-/*   Updated: 2023/11/07 13:13:02 by vlafouas         ###   ########.fr       */
+/*   Updated: 2023/11/07 13:27:28 by vlafouas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,17 @@
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
+	size_t			n_bytes;
 	unsigned char	*output;
+	size_t			i;
 
-	return (output);
+	n_bytes = nmemb * size;
+	output = malloc(n_bytes);
+	i = 0;
+	while (i < n_bytes)
+	{
+		output[i] = 0;
+		i++;
+	}
+	return ((void *)output);
 }
