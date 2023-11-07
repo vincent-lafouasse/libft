7c12dee84aecf0463f1206e81d73a461768bf740
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-07 14:02:54 +0100

    test memset with random values

--- test/src/test_memset.cpp
+++ test/src/test_memset.cpp
@@ -6,28 +6,54 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:34:38 by vlafouas          #+#    #+#             */
-/*   Updated: 2023/11/07 13:39:24 by vlafouas         ###   ########.fr       */
+/*   Updated: 2023/11/07 14:02:19 by vlafouas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <gtest/gtest.h>
+#include <iostream>
+#include <stdlib.h>
+
+#define N_TESTS 420
 
 extern "C"
 {
 #include "libft.h"
 };
 
-void test_memset(void *s, int c, size_t n)
+struct MemsetInput
 {
-	unsigned char* buffer = (unsigned char*)s;
+	int value;
+	size_t n_bytes;
+
+	void set_random() {
+		int is_positive = rand() % 2;
+		int sign = 1 * is_positive + (-1) * (!is_positive);
+		value = rand() % 100000;
+		value *= sign;
+		n_bytes = 1 + rand() % 10000;
+	}
+};
+
+void test_memset(int c, size_t n)
+{
+	unsigned char* buffer = (unsigned char*)malloc(n);
+	ft_memset((void*)buffer, c, n);
 
 	for (size_t i = 0; i < n; i++)
 	{
+		std::cout << rand() << '\n';
 		ASSERT_EQ(buffer[i], c) << "\t error found at index " << i;
 	}
 }
 
 TEST(String, Memset)
 {
-	test_memset(0, 0, 0);
+	MemsetInput in;
+	
+	for (int i = 0; i < N_TESTS; i++)
+	{
+		in.set_random();
+	    test_memset(in.value, in.n_bytes);
+	}
 }
