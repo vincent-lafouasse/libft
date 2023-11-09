9540f5dd3706293bbba8aea7f3de83011bb801d7
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-09 14:46:18 +0100

    unity hello world

---
+++ test/src/main.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+#include "test-framework/unity.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void	setUp(void)
+{
+}
+
+void	tearDown(void)
+{
+}
+
+static void	test_atoi(void)
+{
+	TEST_ASSERT_EQUAL_INT(42, ft_atoi("42"));
+}
+
+int	main(void)
+{
+	UnityBegin("test.c");
+	RUN_TEST(test_atoi);
+	return (UnityEnd());
+}
