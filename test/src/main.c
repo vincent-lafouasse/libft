9c3cb9ffb2c027a3686ae2c8af060d6b8d567cf7
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-09 17:28:49 +0100

    test bzero

--- test/src/main.c
+++ test/src/main.c
@@ -15,6 +15,7 @@ void run_test_strlen(void);
 void run_test_ctype(void);
 void run_test_atoi(void);
 void run_test_memset(void);
+void run_test_bzero(void);
 
 int	main(void)
 {
@@ -23,5 +24,6 @@ int	main(void)
 	run_test_ctype();
 	run_test_atoi();
 	run_test_memset();
+	run_test_bzero();
 	return (UnityEnd());
 }
