bdf6afc99fb7f1a847d3a9cb12efa894c967574d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-09 17:17:14 +0100

    test memset

--- test/src/test_memset.c
+++ test/src/test_memset.c
@@ -7,6 +7,8 @@
 #include <stdlib.h>
 
 #define BUFFER_SIZE 1024
+#define MAX_SIZE 1024
+#define N_TRIALS 420
 
 void scramble_array(void* array, size_t size)
 {
@@ -18,37 +20,71 @@ void scramble_array(void* array, size_t size)
 	}
 }
 
-void try_memset(int value, size_t size)
+typedef struct {
+	int value;
+	size_t size;
+} Input;
+
+void try_memset(Input in)
 {
+	if (in.size == 0)
+		TEST_PASS();
+
 	char error[BUFFER_SIZE];
 	t_byte expected;
 	t_byte actual;
 
-	t_byte* bytes = malloc(size);
-	scramble_array(bytes, size);
+	t_byte* bytes = malloc(in.size);
+	scramble_array(bytes, in.size);
 
-	memset(bytes, value, size);
+	memset(bytes, in.value, in.size);
 
-	for (size_t i = 0; i < size; i++)
+	for (size_t i = 0; i < in.size; i++)
 	{
-		expected = value;
+		expected = in.value;
 		actual = bytes[i];
 
 		if (expected != actual)
 		{
-			sprintf(error, "Error at byte %u, expected 0x%02x was 0x%02x", i, expected, actual);
+			sprintf(error, "Error at byte %zu, expected 0x%02x was 0x%02x", i, expected, actual);
 			TEST_FAIL_MESSAGE(error);
 		}
 	}
 }
 
+Input get_random_input()
+{
+	Input in;
+	
+	in.value = rand();
+	in.size = 1 + rand() % MAX_SIZE;
+	return in;
+}
+
+Input input(int value, size_t size)
+{
+	Input in;
+
+	in.value = value;
+	in.size = size;
+	return in;
+}
+
 void test_memset(void)
 {
-	try_memset(5, 5);
+	try_memset(input(0, 0));
+	try_memset(input(69, 420));
+}
+
+void test_stochastic_memset(void)
+{
+	for (int i = 0; i < N_TRIALS; i++)
+		try_memset(get_random_input());
 }
 
 void run_test_memset(void)
 {
 	printf("\n----- Memset -----\n");
 	RUN_TEST(test_memset);
+	RUN_TEST(test_stochastic_memset);
 }
