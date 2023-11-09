024dd1b83ddce75e83f7368fd42f95a942d203c1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-09 16:27:51 +0100

    remove duplication with function pointers

--- test/src/test_ctype.c
+++ test/src/test_ctype.c
@@ -21,6 +21,26 @@ void compare_isupper(int c)
 	TEST_ASSERT_TRUE_MESSAGE(expected == actual, error);
 }
 
+void compare(int (*my_ft)(int), int (*libc_ft)(int)  ,int c)
+{
+	int expected = (*libc_ft)(c);
+	int actual = (*my_ft)(c);
+
+	char error[BUFFER_SIZE];
+	sprintf(error, "Error for input %c, ie ASCII %d, expected %d was %d", c, c, expected, actual);
+
+	TEST_ASSERT_TRUE_MESSAGE(expected == actual, error);
+}
+
+void test_islower(void)
+{
+	int (*my_ft)(int) = &ft_islower;
+	int (*libc_ft)(int) = &islower;
+
+	for (int c = LOWER_BOUND; c <= UPPER_BOUND; c++)
+		compare(my_ft, libc_ft, c);
+}
+
 void test_isupper(void)
 {
 	for (int c = LOWER_BOUND; c <= UPPER_BOUND; c++)
@@ -31,4 +51,5 @@ void run_test_ctype(void)
 {
 	printf("\n----- Ctype -----\n");
 	RUN_TEST(test_isupper);
+	RUN_TEST(test_islower);
 }
