a3cce8f9d09776754128322304b3f4f8b5c42c49
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-09 16:38:29 +0100

    test atoi

--- test/src/main.c
+++ test/src/main.c
@@ -13,11 +13,13 @@ void	tearDown(void)
 
 void run_test_strlen(void);
 void run_test_ctype(void);
+void run_test_atoi(void);
 
 int	main(void)
 {
 	UnityBegin("Libft");
 	run_test_strlen();
 	run_test_ctype();
+	run_test_atoi();
 	return (UnityEnd());
 }
---
+++ test/src/test_atoi.c
@@ -0,0 +1,44 @@
+#include "libft.h"
+
+#include "test-framework/unity.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+
+#define BUFFER_SIZE 1024
+
+void compare_atoi(const char* s)
+{
+	int expected = atoi(s);
+	int actual = ft_atoi(s);
+
+	char error[BUFFER_SIZE];
+	sprintf(error, "Error for input \"%s\", expected %d was %d", s, expected, actual);
+
+	TEST_ASSERT_TRUE_MESSAGE(expected == actual, error);
+}
+
+void test_atoi_valid(void)
+{
+	compare_atoi("0");
+	compare_atoi("1");
+	compare_atoi("42");
+	compare_atoi("-42");
+	compare_atoi("              \t\n\t 42");
+	compare_atoi("-1");
+}
+
+void test_atoi_invalid(void)
+{
+	compare_atoi("yo yoyo what uppppp");
+	compare_atoi("42 c'est pas mal en fait");
+	compare_atoi("0xcafe");
+	compare_atoi("--------42");
+}
+
+void run_test_atoi(void)
+{
+	printf("\n----- Atoi -----\n");
+	RUN_TEST(test_atoi_valid);
+	RUN_TEST(test_atoi_invalid);
+}
