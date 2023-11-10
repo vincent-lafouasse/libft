1dbd0d31e38a5956607aa6c7fd11375d286d15a1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-10 14:17:38 +0100

    check if malloc output is NULL

--- src/libc/stdlib/ft_calloc.c
+++ src/libc/stdlib/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:12:21 by vlafouas          #+#    #+#             */
-/*   Updated: 2023/11/10 14:13:19 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/10 14:16:56 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,11 @@ void	*ft_calloc(size_t nmemb, size_t size)
 {
 	size_t	n_bytes;
 	t_byte	*output;
-	size_t	i;
 
 	n_bytes = nmemb * size;
-	if (n_bytes == 0)
-		return NULL;
 	output = malloc(n_bytes);
-	i = 0;
-	while (i < n_bytes)
-	{
-		output[i] = 0;
-		i++;
-	}
-	return ((void *)output);
+	if (output == NULL)
+		return (NULL);
+	ft_bzero(output, n_bytes);
+	return (output);
 }
