942a6cf32f53d7366c694257d7ee2fee4f8fe1e4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-10 13:29:59 +0100

    better script for making deliverable repo

--- aux/deliverable_maker.sh
+++ aux/deliverable_maker.sh
@@ -1,10 +1,24 @@
 #! /usr/bin/env bash
 
+
 DIR_NAME='rendu'
 SRC_DIR='./src'
 
+rm -rf "${DIR_NAME}"
 mkdir "${DIR_NAME}"
 
-for c_file in $(find "${SRC_DIR}" -name '*.c'); do
+MANDATORY_C_FILES=
+BONUS_C_FILES=
+
+for c_file in $(find "${SRC_DIR}" -name '*.c' | grep -v bonus); do
+	MANDATORY_C_FILES="${MANDATORY_C_FILES} ${c_file}"
 	cp "${c_file}" "${DIR_NAME}"
 done
+
+for c_file in $(find "${SRC_DIR}" -name '*.c' | grep  bonus); do
+	BONUS_C_FILES="${BONUS_C_FILES} ${c_file}"
+	cp "${c_file}" "${DIR_NAME}"
+done
+
+LIB='./include/libft.h'
+cp "${LIB}" "${DIR_NAME}"
