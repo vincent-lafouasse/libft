b04f5d37cc71d29b51fc53e74d94464d693275c8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-11 18:24:05 +0100

    dummy implementation for test_memcpy

---
+++ test/src/test_memcpy.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+#include "test-framework/unity.h"
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void test_memcpy(void) {}
+
+void run_test_memcpy(void)
+{
+    printf("\n----- Memcpy -----\n");
+    RUN_TEST(test_memcpy);
+}
