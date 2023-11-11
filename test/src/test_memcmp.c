b6c6ac98859e69ca1b0f27f8d64fe79c5afd1032
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-11 18:44:31 +0100

    tests for memcmp

---
+++ test/src/test_memcmp.c
@@ -0,0 +1,14 @@
+#include "libft.h"
+#include "test-framework/unity.h"
+#include "utils.h"
+
+#include <string.h>
+
+static void test_memcmp(void)
+{}
+
+void run_test_memcmp(void)
+{
+    printf("\n----- Memcmp -----\n");
+    RUN_TEST(test_memcmp);
+}
