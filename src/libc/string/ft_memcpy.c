6b7bf8ac444874ab674dcacf7375a01b77b0015d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-11 18:30:09 +0100

    implement memcpy

--- src/libc/string/ft_memcpy.c
+++ src/libc/string/ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 18:18:48 by vlafouas          #+#    #+#             */
-/*   Updated: 2023/11/11 18:20:20 by vlafouas         ###   ########.fr       */
+/*   Updated: 2023/11/11 18:29:51 by vlafouas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,17 @@
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	(void)n;
-	(void)src;
+	size_t	i;
+	t_byte	*dest_bytes;
+	t_byte	*src_bytes;
+
+	i = 0;
+	dest_bytes = (t_byte *)dest;
+	src_bytes = (t_byte *)src;
+	while (i < n)
+	{
+		dest_bytes[i] = src_bytes[i];
+		i++;
+	}
 	return (dest);
 }
