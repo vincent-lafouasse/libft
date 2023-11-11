50e6b026990641250b0dddcd5cd9d433fe6f365f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-11 18:58:08 +0100

    cast void* to t_byte* in debug print

--- test/src/test_memcmp.c
+++ test/src/test_memcmp.c
@@ -13,7 +13,7 @@ static void compare_memcmp(const void* s1, const void* s2, size_t n)
 	int libc_res = memcmp(s1, s2, n);
 
 	char error[BUFFER_SIZE];
-	sprintf(error, "Error for input \"%s\". \"%s\", %zu. ft_memcmp returned %d while memcmp returned %d", s1, s2, n, my_res, libc_res);
+	sprintf(error, "Error for input \"%s\". \"%s\", %zu. ft_memcmp returned %d while memcmp returned %d", (t_byte*)s1, (t_byte*)s2, n, my_res, libc_res);
 
 	TEST_ASSERT_TRUE_MESSAGE(sign(my_res) == sign(libc_res), error);
 }
