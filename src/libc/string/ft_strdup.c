c6193d4911e9598a50c986bd118bcc27418c5e40
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-12 16:29:03 +0100

    remove dead code

--- src/libc/string/ft_strdup.c
+++ src/libc/string/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 17:38:57 by vlafouas          #+#    #+#             */
-/*   Updated: 2023/11/11 18:33:27 by vlafouas         ###   ########.fr       */
+/*   Updated: 2023/11/12 16:25:20 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,9 @@
 char	*ft_strdup(const char *s)
 {
 	char	*out;
-	int		i;
 	size_t	len;
 
 	len = ft_strlen(s);
-	i = 0;
 	out = malloc(1 + len);
 	if (!out)
 		return (NULL);
