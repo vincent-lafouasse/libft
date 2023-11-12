210d6cdbfd7d7895ac3c7e7af568c8cbcde70251
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-12 17:15:05 +0100

    implement strncmp

--- src/libc/string/ft_strncmp.c
+++ src/libc/string/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 17:07:40 by poss              #+#    #+#             */
-/*   Updated: 2023/11/12 17:07:41 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/12 17:14:25 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,11 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	(void)s1;
-	(void)s2;
-	(void)n;
-	return (0);
+	while (*s1 == *s2 && n > 1 && *s1 && *s2)
+	{
+		s1++;
+		s2++;
+		n--;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
 }
