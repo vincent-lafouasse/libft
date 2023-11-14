7d809ad154afa5c04f3dd1cab5552cf77b9c3955
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-14 16:23:36 +0100

    implement test for strlcat

--- test/src/test_strlcat.c
+++ test/src/test_strlcat.c
@@ -16,6 +16,27 @@ static void compare_strlcat(const char* src,
                             const char* dest,
                             size_t buffer_size)
 {
+    char* my_buffer = malloc(buffer_size);
+    char* libc_buffer = malloc(buffer_size);
+    strlcpy(my_buffer, src, buffer_size);
+    strlcpy(libc_buffer, src, buffer_size);
+
+    size_t my_return = ft_strlcat(my_buffer, src, buffer_size);
+    size_t libc_return = strlcat(my_buffer, src, buffer_size);
+
+    char error[BUFFER_SIZE];
+
+    sprintf(
+        error,
+        "Error with return value with input \"%s\", \"%s\" and %zu, ft_strlcat "
+        "returned %zu and strlcat returned %zu",
+        dest, src, buffer_size, my_return, libc_return);
+    TEST_ASSERT_TRUE_MESSAGE(my_return == libc_return, error);
+
+    compare_bytes((t_byte*)libc_buffer, (t_byte*)my_buffer, buffer_size);
+
+    free(my_buffer);
+    free(libc_buffer);
 }
 
 static void test_strlcat(void) {}
