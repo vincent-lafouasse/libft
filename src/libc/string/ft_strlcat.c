b5b2a0ad5cd2090c8ce5b5cd6b0e285f6af6dc38
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-14 17:24:55 +0100

    minor refactor

--- src/libc/string/ft_strlcat.c
+++ src/libc/string/ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 16:11:11 by poss              #+#    #+#             */
-/*   Updated: 2023/11/14 17:22:21 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/14 17:24:09 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ static size_t min(size_t, size_t);
 size_t	ft_strlcat(char *dst, const char *src, size_t buffer_size)
 {
 	size_t src_len = ft_strlen(src);
-	size_t dst_len = min(buffer_size, ft_strlen(dst));
+	size_t dst_len = ft_strlen(dst);
 
-	if (dst_len == buffer_size)
+	if (dst_len > buffer_size)
 	{
 		return buffer_size + src_len;
 	}
