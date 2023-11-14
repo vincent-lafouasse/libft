7856743984595372f7128e3e74a434480e3c72a2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-14 17:07:30 +0100

    minor refactor

--- src/libc/string/ft_strlcat.c
+++ src/libc/string/ft_strlcat.c
@@ -6,23 +6,23 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 16:11:11 by poss              #+#    #+#             */
-/*   Updated: 2023/11/14 16:58:28 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/14 17:07:12 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-size_t	ft_strlcat(char *dst, const char *src, size_t size)
+size_t	ft_strlcat(char *dst, const char *src, size_t buffer_size)
 {
 	size_t	i;
 
 	i = ft_strlen(dst);
-	if (i >= size)
+	if (i >= buffer_size)
 	{
-		return (i + size);
+		return (i + buffer_size);
 	}
-	while (i < size - 1 && *src)
+	while (i < buffer_size - 1 && *src)
 	{
 		dst[i] = *src;
 		src++;
