4a1156676f1fa9c16c2de8e5687d75b4281d2afd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-14 17:22:50 +0100

    hmmm

--- src/libc/string/ft_strlcat.c
+++ src/libc/string/ft_strlcat.c
@@ -6,28 +6,39 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 16:11:11 by poss              #+#    #+#             */
-/*   Updated: 2023/11/14 17:07:12 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/14 17:22:21 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
+static size_t min(size_t, size_t);
+
 size_t	ft_strlcat(char *dst, const char *src, size_t buffer_size)
 {
-	size_t	i;
+	size_t src_len = ft_strlen(src);
+	size_t dst_len = min(buffer_size, ft_strlen(dst));
 
-	i = ft_strlen(dst);
-	if (i >= buffer_size)
+	if (dst_len == buffer_size)
 	{
-		return (i + buffer_size);
+		return buffer_size + src_len;
 	}
-	while (i < buffer_size - 1 && *src)
+	if (buffer_size > dst_len + src_len)
 	{
-		dst[i] = *src;
-		src++;
-		i++;
+		ft_memcpy(dst + dst_len, src, 1 + src_len);
 	}
-	dst[i] = 0;
-	return (ft_strlen(src) + i);
+	else
+	{
+		
+	}
+	return dst_len + src_len;
+}
+
+static size_t min(size_t a, size_t b)
+{
+	if (a < b)
+	return a;
+	else
+	return b;
 }
