b48a5a606b319bb1b14232c98962eab0f7402f15
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-14 17:36:18 +0100

    implement strlcat

--- src/libc/string/ft_strlcat.c
+++ src/libc/string/ft_strlcat.c
@@ -6,39 +6,51 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 16:11:11 by poss              #+#    #+#             */
-/*   Updated: 2023/11/14 17:24:09 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/14 17:35:57 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-static size_t min(size_t, size_t);
+static size_t	min(size_t, size_t);
+
+/* an explanation
+ *
+ * there are 3 cases:
+ *
+ * - the buffer can't hold dst
+ * - the buffer can hold dst but not dst + src
+ * - the buffer can hold both
+ */
 
 size_t	ft_strlcat(char *dst, const char *src, size_t buffer_size)
 {
-	size_t src_len = ft_strlen(src);
-	size_t dst_len = ft_strlen(dst);
+	size_t	src_len;
+	size_t	dst_len;
 
+	src_len = ft_strlen(src);
+	dst_len = ft_strlen(dst);
 	if (dst_len > buffer_size)
 	{
-		return buffer_size + src_len;
+		return (buffer_size + src_len);
 	}
-	if (buffer_size > dst_len + src_len)
+	if (dst_len + src_len < buffer_size)
 	{
 		ft_memcpy(dst + dst_len, src, 1 + src_len);
 	}
 	else
 	{
-		
+		ft_memcpy(dst + dst_len, src, buffer_size - dst_len - 1);
+		dst[buffer_size - 1] = '\0';
 	}
-	return dst_len + src_len;
+	return (dst_len + src_len);
 }
 
-static size_t min(size_t a, size_t b)
+static size_t	min(size_t a, size_t b)
 {
 	if (a < b)
-	return a;
+		return (a);
 	else
-	return b;
+		return (b);
 }
