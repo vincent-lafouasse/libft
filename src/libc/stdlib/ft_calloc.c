c5547bdf9393c1c8035e3f4a533a1a34ca93c061
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-21 16:00:07 +0100

    add sanity check to ft_calloc to check for overflow

--- src/libc/stdlib/ft_calloc.c
+++ src/libc/stdlib/ft_calloc.c
@@ -6,17 +6,24 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 13:12:21 by vlafouas          #+#    #+#             */
-/*   Updated: 2023/11/10 14:21:33 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/21 15:58:09 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
+/* * includes a sanity check to ensure that there was no overflow
+ */
+
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	t_byte	*output;
 
+	if (size != 0 && (nmemb * size) / size != nmemb)
+	{
+		return (NULL);
+	}
 	output = malloc(nmemb * size);
 	if (output == NULL)
 		return (NULL);
