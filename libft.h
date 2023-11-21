a9396c300fc22e184e39a6dea773ff6257fc0341
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-21 18:32:34 +0100

    format

--- libft.h
+++ libft.h
@@ -42,7 +42,8 @@ void					*ft_memcpy(void *dest, const void *src, size_t n);
 int						ft_memcmp(const void *s1, const void *s2, size_t n);
 int						ft_strncmp(const char *s1, const char *s2, size_t n);
 void					*ft_memchr(const void *s, int c, size_t n);
-char* ft_strnstr(const char* haystack, const char* needle, size_t len);
+char					*ft_strnstr(const char *haystack, const char *needle,
+							size_t len);
 
 /* ----- from `strings.h` --------------------------------------------------- */
 void					ft_bzero(void *s, size_t n);
