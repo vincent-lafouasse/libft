b5a2e71c466567e5f6cfa466c6cb9c3044f566a8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-21 20:05:59 +0100

    refactor

--- src/libc/string/ft_strnstr.c
+++ src/libc/string/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 18:24:46 by poss              #+#    #+#             */
-/*   Updated: 2023/11/21 19:28:13 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/21 20:05:23 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	last_index;
+	size_t	needle_len;
 	size_t	i;
 
-	last_index = ft_strlen(haystack) - len;
+	needle_len = ft_strlen(needle);
 	i = 0;
-	while (i < last_index)
+	while (i < len)
 	{
-		if (!ft_memcmp(haystack + i, needle, 1 + ft_strlen(needle)))
+		if (!ft_memcmp(haystack + i, needle, needle_len))
 		{
 			return ((char *)(haystack + i));
 		}
