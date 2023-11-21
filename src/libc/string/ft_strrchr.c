21208d4cfe8d04107d9938e492c8fb56b733f112
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-21 15:59:50 +0100

    minor refactor

--- src/libc/string/ft_strrchr.c
+++ src/libc/string/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 14:37:20 by poss              #+#    #+#             */
-/*   Updated: 2023/11/10 16:46:07 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/21 15:36:18 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strrchr(const char *s, int c)
 	i = ft_strlen(s);
 	while (i >= 0)
 	{
-		if (s[i] == c)
+		if (s[i] == (unsigned char)c)
 		{
 			return ((char *)s + i);
 		}
