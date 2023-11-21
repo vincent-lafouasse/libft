0d5cdb5e53f673226c72256dd81717d3514b26b3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-21 19:28:38 +0100

    format

--- src/libc/string/ft_strnstr.c
+++ src/libc/string/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 18:24:46 by poss              #+#    #+#             */
-/*   Updated: 2023/11/21 18:24:58 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/21 19:28:13 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	last_index;
+	size_t	i;
+
+	last_index = ft_strlen(haystack) - len;
+	i = 0;
+	while (i < last_index)
+	{
+		if (!ft_memcmp(haystack + i, needle, 1 + ft_strlen(needle)))
+		{
+			return ((char *)(haystack + i));
+		}
+		i++;
+	}
 	return (0);
 }
