6560974f1595a8fc432e20834ff0838e34ac7b08
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-22 14:13:24 +0100

    implement strjoin

--- src/extension/string/ft_strjoin.c
+++ src/extension/string/ft_strjoin.c
@@ -6,13 +6,26 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 14:07:44 by poss              #+#    #+#             */
-/*   Updated: 2023/11/22 14:07:54 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/22 14:11:24 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	return (0);
+	size_t	len1;
+	size_t	len2;
+	char	*out;
+
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	out = malloc(len1 + len2 + 1);
+	if (!out)
+		return (NULL);
+	ft_memcpy(out, s1, len1);
+	ft_memcpy(out + len1, s2, len2);
+	out[len1 + len2 + 1] = '\0';
+	return (out);
 }
