93ee5ec263fa58f97166b407392425b90a1d38dd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-22 16:13:54 +0100

    implement itoa

---
+++ src/extension/string/ft_itoa.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/22 15:37:48 by poss              #+#    #+#             */
+/*   Updated: 2023/11/22 16:11:53 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+#include <stdlib.h>
+
+#define TESTING 0
+
+unsigned int	ft_abs(int n);
+
+static size_t	get_n_digits(unsigned int n)
+{
+	size_t	n_digits;
+
+	n_digits = 0;
+	while (n)
+	{
+		n /= 10;
+		n_digits++;
+	}
+	return (n_digits);
+}
+
+void	invert_fill(char *buffer, int n)
+{
+	unsigned int	abs;
+
+	abs = ft_abs(n);
+	while (abs)
+	{
+		*buffer = '0' + (abs % 10);
+		abs /= 10;
+		buffer++;
+	}
+	if (n < 0)
+		*buffer = '-';
+}
+
+unsigned int	ft_abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
+
+void	mem_revert(void *s, size_t len)
+{
+	size_t	i;
+	t_byte	mem;
+	t_byte	*bytes;
+
+	i = 0;
+	bytes = s;
+	while (i < len / 2)
+	{
+		mem = bytes[i];
+		bytes[i] = bytes[len - 1 - i];
+		bytes[len - 1 - i] = mem;
+		i++;
+	}
+}
+
+char	*ft_itoa(int n)
+{
+	char	*out;
+	size_t	len;
+
+	if (n == 0)
+		return (ft_strdup("0"));
+	len = get_n_digits(ft_abs(n)) + (n < 0);
+	out = malloc(1 + len);
+	if (!out)
+		return (NULL);
+	invert_fill(out, n);
+	mem_revert(out, len);
+	return (out);
+}
+
+#if TESTING
+
+# include <limits.h>
+# include <stdio.h>
+
+int	main(void)
+{
+	char	*s;
+
+	s = ft_itoa(-1);
+	printf("%s\n", s);
+}
+#endif
