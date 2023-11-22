58d76ff5bfccba0a2af536b7414f76c531cba077
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-22 14:25:48 +0100

    implement substr

--- src/extension/string/ft_substr.c
+++ src/extension/string/ft_substr.c
@@ -6,13 +6,25 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 14:14:45 by poss              #+#    #+#             */
-/*   Updated: 2023/11/22 14:14:55 by poss             ###   ########.fr       */
+/*   Updated: 2023/11/22 14:22:01 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdlib.h>
 
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
-	return (0);
+	char	*out;
+
+	out = malloc(1 + len);
+	if (!out)
+		return (NULL);
+	if (start >= ft_strlen(s))
+	{
+		*out = '\0';
+		return (out);
+	}
+	ft_strlcpy(out, s + start, 1 + len);
+	return (out);
 }
