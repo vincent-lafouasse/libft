970a4a531f1908411ea4fb13bc4518ff4e97a4e8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-11-29 17:42:20 +0100

    minor refactor

--- aux/deliverable_maker.sh
+++ aux/deliverable_maker.sh
@@ -3,27 +3,33 @@
 DIR_NAME=rendu
 SRC_DIR=./src
 
-rm -rf "${DIR_NAME}"
-mkdir "${DIR_NAME}"
+make_rendu() {
+	rm -rf "${DIR_NAME}"
+	mkdir "${DIR_NAME}"
 
-MANDATORY_C_FILES__=
-BONUS_C_FILES__=
+	MANDATORY_C_FILES__=
+	BONUS_C_FILES__=
 
-for c_file in $(find "${SRC_DIR}" -name '*.c' | grep -v lst); do
-	MANDATORY_C_FILES__+=" $(basename "${c_file}")"
-	cp "${c_file}" "${DIR_NAME}"
-done
+	for c_file in $(find "${SRC_DIR}" -name '*.c' | grep -v lst); do
+		MANDATORY_C_FILES__+=" $(basename "${c_file}")"
+		cp "${c_file}" "${DIR_NAME}"
+	done
 
-for c_file in $(find "${SRC_DIR}" -name '*.c' | grep lst); do
-	BONUS_C_FILES__+=" $(basename "${c_file}")"
-	cp "${c_file}" "${DIR_NAME}"
-done
+	for c_file in $(find "${SRC_DIR}" -name '*.c' | grep lst); do
+		BONUS_C_FILES__+=" $(basename "${c_file}")"
+		cp "${c_file}" "${DIR_NAME}"
+	done
 
-LIB='./libft.h'
-cp "${LIB}" "${DIR_NAME}"
+	LIB='./libft.h'
+	cp "${LIB}" "${DIR_NAME}"
 
-TEMPLATE_MAKEFILE='./aux/Makefile'
-TARGET_MAKEFILE="${DIR_NAME}/Makefile"
+	TEMPLATE_MAKEFILE='./aux/Makefile'
+	TARGET_MAKEFILE="${DIR_NAME}/Makefile"
 
-export MANDATORY_C_FILES__ BONUS_C_FILES__
-envsubst <"${TEMPLATE_MAKEFILE}" >"${TARGET_MAKEFILE}"
+	export MANDATORY_C_FILES__ BONUS_C_FILES__
+	envsubst <"${TEMPLATE_MAKEFILE}" >"${TARGET_MAKEFILE}"
+}
+
+################################################################################
+
+make_rendu
