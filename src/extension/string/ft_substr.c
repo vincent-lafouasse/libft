9123ffa782ada8c4ce8ef397449d6251ba38a534
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-01 17:17:48 +0100

    minor refactor

--- src/extension/string/ft_substr.c
+++ src/extension/string/ft_substr.c
@@ -6,25 +6,38 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/22 14:14:45 by poss              #+#    #+#             */
-/*   Updated: 2023/11/22 14:22:01 by poss             ###   ########.fr       */
+/*   Updated: 2023/12/01 17:17:36 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdlib.h>
 
+static size_t	min(size_t a, size_t b);
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*out;
 
-	out = malloc(1 + len);
-	if (!out)
-		return (NULL);
 	if (start >= ft_strlen(s))
 	{
+		out = malloc(1);
+		if (!out)
+			return (NULL);
 		*out = '\0';
 		return (out);
 	}
+	len = min(len, ft_strlen(s + start));
+	out = malloc(1 + len);
+	if (!out)
+		return (NULL);
 	ft_strlcpy(out, s + start, 1 + len);
 	return (out);
+}
+
+static size_t	min(size_t a, size_t b)
+{
+	if (a < b)
+		return (a);
+	return (b);
 }
