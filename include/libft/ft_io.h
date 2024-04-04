1983710a10198744656dc33e48c4d44f28abbc0b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-04 13:55:00 +0200

    add declarations for gnl and ft_printf

--- include/libft/ft_io.h
+++ include/libft/ft_io.h
@@ -6,12 +6,15 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/30 10:15:37 by poss              #+#    #+#             */
-/*   Updated: 2024/03/30 10:15:54 by poss             ###   ########.fr       */
+/*   Updated: 2024/04/04 13:54:45 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_IO_H
 # define FT_IO_H
+
+char	*get_next_line(int fd);
+int	ft_printf(const char *format, ...);
 
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(const char *s, int fd);
